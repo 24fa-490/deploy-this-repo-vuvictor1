4f28fde800daa1e58aec4686d89e7f57ac8ede3a
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2024-10-17 19:14:02 -0700

    port fixes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import mkcert from "vite-plugin-mkcert";
+import mkcert from "vite-plugin-mkcert"
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+
 export default defineConfig({
-	plugins: [
-		sveltekit(),
+	plugins: [sveltekit(),
 		mkcert()
 	],
 	server: {
-		https: true, 
 		host: '0.0.0.0', 
-		port: process.env.PORT || 5173,  
-		proxy: {},  
+		port: process.env.PORT || 5173, 
+		https: process.env.NODE_ENV === 'development', 
+		proxy: {}, // essential to avoid "can't use Symbol where you need a string" error
 	},
-});
+})
